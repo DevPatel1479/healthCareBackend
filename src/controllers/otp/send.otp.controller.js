9d5e2f389683f3ec77938163bb4d397900064d4c
Dev Patel <devpatel1828king@gmail.com>
2026-04-16 21:25:26 +0530

    added whatsapp otp argument in send otp

--- src/controllers/otp/send.otp.controller.js
+++ src/controllers/otp/send.otp.controller.js
@@ -2,15 +2,18 @@ import axios from "axios";
 import { generateOTP, saveOTP } from "../../services/otp.service.js";
 
 
+import axios from "axios";
+import { generateOTP, saveOTP } from "../../services/otp.service.js";
+
 export const sendOTP = async (req, res) => {
   try {
-    const { phone } = req.body;
+    const { phone, type } = req.body; // type = 'sms' | 'whatsapp'
 
     // Validation
-    if (!phone) {
+    if (!phone || !type) {
       return res.status(400).json({
         success: false,
-        message: 'Phone number is required',
+        message: 'Phone and type (sms/whatsapp) are required',
       });
     }
 
@@ -21,21 +24,39 @@ export const sendOTP = async (req, res) => {
       });
     }
 
+    if (!['sms', 'whatsapp'].includes(type)) {
+      return res.status(400).json({
+        success: false,
+        message: 'Invalid type. Use sms or whatsapp',
+      });
+    }
+
     const otp = generateOTP();
 
-    saveOTP(phone, otp);
+    await saveOTP(phone, otp);
 
-    const message = encodeURIComponent(
-      `Dear Member, Login using your mobile number and One Time Password ${otp}. Do Not disclose it to anyone - TFMLAB`
-    );
+    let apiUrl = '';
 
-    const smsUrl = `${process.env.SMS_BASE_URL}?APIkey=${process.env.SMS_API_KEY}&SenderID=${process.env.SMS_SENDER_ID}&SMSType=${process.env.SMS_TYPE}&Mobile=${phone}&MsgText=${message}&EntityID=${process.env.SMS_ENTITY_ID}&TemplateID=${process.env.SMS_TEMPLATE_ID}`;
+    // 📩 SMS FLOW
+    if (type === 'sms') {
+      const message = encodeURIComponent(
+        `Dear Member, Login using your mobile number and One Time Password ${otp}. Do Not disclose it to anyone - TFMLAB`
+      );
 
-    await axios.get(smsUrl, { timeout: 5000 });
+      apiUrl = `${process.env.SMS_BASE_URL}?APIkey=${process.env.SMS_API_KEY}&SenderID=${process.env.SMS_SENDER_ID}&SMSType=${process.env.SMS_TYPE}&Mobile=${phone}&MsgText=${message}&EntityID=${process.env.SMS_ENTITY_ID}&TemplateID=${process.env.SMS_TEMPLATE_ID}`;
+    }
+
+    // 💬 WHATSAPP FLOW
+    if (type === 'whatsapp') {
+      apiUrl = `${process.env.WT_BASE_URL}?authTkn=${process.env.WT_AUTHTOKEN}&templateName=${process.env.WT_TEMPLATE_NAME}&mobileNo=${phone}&otp=${otp}`;
+    }
+
+    // Call API
+    await axios.get(apiUrl, { timeout: 5000 });
 
     return res.status(200).json({
       success: true,
-      message: 'OTP sent successfully',
+      message: `OTP sent via ${type}`,
       otp: process.env.NODE_ENV === 'development' ? otp : undefined,
     });
 
