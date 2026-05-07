b032b79061f4da6b90c384d209029025a226bcad
Dev Patel <devpatel1828king@gmail.com>
2026-05-08 00:04:14 +0530

    fixed the

--- src/routes/caregiver.routes.js
+++ src/routes/caregiver.routes.js
@@ -4,8 +4,8 @@ import express from 'express'
 import { getCaregiverTasks } from '../controllers/caregiver/caregiver.tasks.controller.js';
 import { updateTaskStatus } from '../controllers/caregiver/caregiver.update.tasks.controller.js';
 import { getAllCaregivers } from '../controllers/caregiver/get.all.caregivers.controller.js';
-import { getCaregiverOverallReport } from '../controllers/caregiver/caregiver.overall.report.controller.js';
-import { getCaregiverPatientWiseReport } from '../controllers/caregiver/caregiver.patient.wise.report.controller.js';
+import { getCaregiverOverallReport, getCaregiverPatientWiseReport } from '../controllers/caregiver/caregiver.report.controller.js';
+
 const router = express.Router()
 
 router.get("/caregiver/:id/tasks", getCaregiverTasks);
