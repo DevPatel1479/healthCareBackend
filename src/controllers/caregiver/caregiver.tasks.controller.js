ca47318f92f7844a96e2d81fbd415c152098ed11
Dev Patel <devpatel1828king@gmail.com>
2026-05-27 20:25:10 +0530

    fixed caregiverid

--- src/controllers/caregiver/caregiver.tasks.controller.js
+++ src/controllers/caregiver/caregiver.tasks.controller.js
@@ -2,7 +2,7 @@ import prisma from "../../lib/prisma.js";
 
 export const getCaregiverTasks = async (req, res) => {
   try {
-    const caregiverId = Number(req.params.caregiverId);
+    const caregiverId = Number(req.params.id);
     console.log("Received caregiver ID:", caregiverId); // Debug log
     if (!caregiverId || isNaN(caregiverId)) {
       return res.status(400).json({
